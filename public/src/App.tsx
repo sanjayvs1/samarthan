f1a606c02afd249a6bef728a5bcad7055567e997
HARSHB0810 <138614246+HARSHB0810@users.noreply.github.com>
2024-09-21 13:25:52 +0530

    Created ass -harsh

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -14,6 +14,7 @@ import SelfProfile from "./pages/SelfProfile";
 import Profiles from "./pages/Profiles";
 import SwitchTest from "./pages/SwitchTest";
 import CreateMeeting from "./pages/CreateMeeting";
+import CreateAssignment from "./pages/CreateAsssignment";
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
           <Route path="/SwitchTest" element={<SwitchTest />} />
           <Route path="/create-room" element={<CreateMeeting />} />
           <Route path="/" element={<Login />} />
+          <Route path="/admin/create-assignment" element={<CreateAssignment/>} />
           
         </Routes>
       </Router>
